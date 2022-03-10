39993ff697e4819cbc1de3f35c11f36106049827
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-03-10 11:21:12 -0800

    Changed Database Settings + Created song_list App

--- songs/settings.py
+++ songs/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%faj1(-#3o5d68s1u4veo45v%vy#y8q2s*5rlbafj+1%imy$(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.corsheaders',  # added via framework
+    'corsheaders',  # added via framework
 ]
 
 MIDDLEWARE = [
@@ -70,17 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'songs.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -125,3 +112,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True  # added via framework
+
+try:
+    from songs.local_settings import *
+except ImportError:
+    pass

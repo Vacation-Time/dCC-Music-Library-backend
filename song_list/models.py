8bc9e97abbe58fb7235eed5c77989890c01e193e
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-03-10 14:33:57 -0800

    Registered Songs Model With Admin Site + Completed POST Functionality

--- song_list/models.py
+++ song_list/models.py
@@ -7,6 +7,5 @@ class Songs(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    # or 'Datefield(null=True)' was also suggested #
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
